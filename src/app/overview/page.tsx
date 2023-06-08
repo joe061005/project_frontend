4017b88a9807da7c6147f3d6c602987b832589c1
joe061005 <72422823+joe061005@users.noreply.github.com>
2023-06-08 20:30:07 +0800

    change web icon

--- src/app/overview/page.tsx
+++ src/app/overview/page.tsx
@@ -12,16 +12,20 @@ export type StockPriceInfo = {
     t: Number;
     v: Number;
     vw: Number;
-  }
+  }[]
   
-  interface StockPriceProps{
-    stockPriceInfo: StockPriceInfo[]
-  }
+  // interface StockPrice{
+  //   stockPriceInfo: StockPriceInfo[]
+  // }
 
-const Overview = ({stockPriceInfo}: StockPriceProps) => {
+const Overview = async () => {
+
+  // const dynamicData: StockPriceInfo = await fetch("https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/2023-06-02?adjusted=true&apiKey=Gh8xZ543zMBDukzN4CNcYoZYtbmRM7fC", {cache: 'no-store' }).then((res) => { console.log(res.json<StockPriceInfo>()); return res.json()})
+  
+
   return (
     <div>
-        <StockTable stockPriceInfo={stockPriceInfo} />
+        {/* <StockTable stockPriceInfo={stockPriceInfo} /> */}
     </div>
   )
 }
